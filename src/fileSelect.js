6a87d6a0063761274f4f7f52548118d51c35f188
kevin <kcomas99@gmail.com>
2016-05-26 23:54:36 -0400

    begun the main react component

---
+++ src/fileSelect.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+/**
+ * The main component of the app
+ */
+class FileSelect extends React.Component {
+
+}
+
+/**
+ * The default propTypes
+ * @type {Object}
+ */
+FileSelect.propTypes = {};
+
+/**
+ * The default props
+ * @type {Object}
+ */
+FileSelect.defaultProps = {};
+
+export default FileSelect;
